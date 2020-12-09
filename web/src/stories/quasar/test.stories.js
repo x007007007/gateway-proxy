5c069f3f74010e9779e97b09afc3cc3b2e67035f
xxc <x007007007@hotmail.com>
2020-12-09 17:23:49 +0800

    update test stories

--- web/src/stories/quasar/test.stories.js
+++ web/src/stories/quasar/test.stories.js
@@ -1,19 +1,46 @@
-
 import { QLayout, QPageContainer, QPage, QSelect, QBtn } from 'quasar'
 
 export default {
   title: 'Quasar'
 }
 
-export const Components = () => ({
-  title: 'QuasarComponents',
+export const qInput = () => ({
+  title: 'q-input',
   components: { QLayout, QPageContainer, QPage, QSelect, QBtn },
   template: `<q-layout>
     <q-page-container>
       <q-page class="full-height full-width justify-center items-center q-pa-xl">
         <div class="col-auto">
           <q-input v-model="name" label="Full name" />
-          <q-select v-model="role" :options="options" label="User Role" />
+          <q-list>
+            <q-item>q-input: {{ name }}</q-item>
+          </q-list>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>`,
+  data () {
+    return {
+      name: null,
+      selected: null,
+      role: 'User',
+      options: ['Admin', 'Supervisor', 'User']
+    }
+  }
+})
+
+export const qSelect = () => ({
+  title: 'q-select',
+  components: { QLayout, QPageContainer, QPage, QSelect, QBtn },
+  template: `<q-layout>
+    <q-page-container>
+      <q-page class="full-height full-width justify-center items-center q-pa-xl">
+        <div class="col-auto">
+          <q-select v-model="name" options="options">
+          </q-select>
+          <q-list>
+            <q-item>q-input: {{ name }}</q-item>
+          </q-list>
         </div>
       </q-page>
     </q-page-container>
